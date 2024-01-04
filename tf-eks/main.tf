f1fbeaa704da211afddd2aec67c11100f58de65c
junghoon2 <erdia22@gmail.com>
2024-01-04 17:45:48 +0900

    feat: remove ipv6 from eks install tf code

--- tf-eks/main.tf
+++ tf-eks/main.tf
@@ -127,15 +127,7 @@ module "vpc" {
   intra_subnets   = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 8, k + 52)]
 
   enable_nat_gateway     = true
-  enable_ipv6            = true
   create_egress_only_igw = true
-
-  public_subnet_ipv6_prefixes                    = [0, 1, 2]
-  public_subnet_assign_ipv6_address_on_creation  = true
-  private_subnet_ipv6_prefixes                   = [3, 4, 5]
-  private_subnet_assign_ipv6_address_on_creation = true
-  intra_subnet_ipv6_prefixes                     = [6, 7, 8]
-  intra_subnet_assign_ipv6_address_on_creation   = true
 
   public_subnet_tags = {
     "kubernetes.io/role/elb" = 1

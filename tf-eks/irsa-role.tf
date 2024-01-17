7e8ac3fec726ac0de3f0841ad27cbd989e249670
junghoon2 <erdia22@gmail.com>
2024-01-17 17:49:02 +0900

    feat: add irsa role

---
+++ tf-eks/irsa-role.tf
@@ -0,0 +1,32 @@
+module "load_balancer_controller_irsa_role" {
+  source = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
+
+  role_name                              = "load-balancer-controller-jerry-dev"
+  attach_load_balancer_controller_policy = true
+
+  oidc_providers = {
+    ex = {
+      provider_arn               = module.eks.oidc_provider_arn
+      namespace_service_accounts = ["kube-system:aws-load-balancer-controller"]
+    }
+  }
+
+  tags = local.tags
+}
+
+module "external_dns_irsa_role" {
+  source = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
+
+  role_name                     = "external-dns-jerry-dev"
+  attach_external_dns_policy    = true
+  external_dns_hosted_zone_arns = ["arn:aws:route53:::hostedzone/${var.secret_hosted_zone_id}"]
+
+  oidc_providers = {
+    ex = {
+      provider_arn               = module.eks.oidc_provider_arn
+      namespace_service_accounts = ["kube-system:external-dns"]
+    }
+  }
+
+  tags = local.tags
+}
---
+++ tf-eks/variables.tf
@@ -0,0 +1,7 @@
+variable "secret_aws_account_id" {
+  type = string
+}
+
+variable "secret_hosted_zone_id" {
+  type = string
+}

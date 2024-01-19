ba64c1be5ec25c0c6371789233aaf3a3e188e4b0
junghoon2 <erdia22@gmail.com>
2024-01-20 04:46:40 +0900

    feat: add lb controller helm chart

--- tf-eks/main.tf
+++ tf-eks/main.tf
@@ -17,12 +17,19 @@ module "eks" {
   cluster_addons = {
     coredns = {
       most_recent = true
+      resolve_conflicts_on_create = true
+      resolve_conflicts_on_update = true
     }
     kube-proxy = {
       most_recent = true
+      resolve_conflicts_on_create = true
+      resolve_conflicts_on_update = true
     }
     vpc-cni = {
       most_recent              = true
+      resolve_conflicts_on_create = true
+      resolve_conflicts_on_update = true
+
       before_compute           = true
       service_account_role_arn = module.vpc_cni_irsa.iam_role_arn
       configuration_values = jsonencode({

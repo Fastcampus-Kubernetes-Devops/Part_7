8d146659cf625c470389e21f84feb7b489067b07
Jerry-levvels <jerry@levvels.io>
2024-07-05 14:19:05 +0900

    fix: comment on eks addon irsa

--- tf-eks/eks-addon.tf
+++ tf-eks/eks-addon.tf
@@ -1,14 +1,14 @@
-# 최초 설치 시 포함하지 않는 것을 권고합니다.
+# # 최초 설치 시 포함하지 않는 것을 권고합니다.
 
-resource "aws_eks_addon" "aws_ebs_csi_driver" {
-  cluster_name             = module.eks.cluster_name
-  addon_name               = "aws-ebs-csi-driver"
-  service_account_role_arn = "arn:aws:iam::${var.secret_aws_account_id}:role/ebs-csi-${var.cluster_name}"
-  addon_version            = "v1.26.1-eksbuild.1" 
-}
+# resource "aws_eks_addon" "aws_ebs_csi_driver" {
+#   cluster_name             = module.eks.cluster_name
+#   addon_name               = "aws-ebs-csi-driver"
+#   service_account_role_arn = "arn:aws:iam::${var.secret_aws_account_id}:role/ebs-csi-${var.cluster_name}"
+#   addon_version            = "v1.26.1-eksbuild.1" 
+# }
 
-resource "aws_eks_addon" "aws_snapshot_controller" {
-  cluster_name             = module.eks.cluster_name
-  addon_name               = "snapshot-controller"
-  # addon_version            = "v1.26.1-eksbuild.1" # 지정하지 않으면 자동으로 default 버전을 설치합니다.
-}
+# resource "aws_eks_addon" "aws_snapshot_controller" {
+#   cluster_name             = module.eks.cluster_name
+#   addon_name               = "snapshot-controller"
+#   # addon_version            = "v1.26.1-eksbuild.1" # 지정하지 않으면 자동으로 default 버전을 설치합니다.
+# }
--- tf-eks/irsa-role.tf
+++ tf-eks/irsa-role.tf
@@ -1,56 +1,56 @@
-# 최초 설치 시 포함하지 않는 것을 권고합니다.
-# 아래 TF 변수(ex var.cluster_name, var.secret_hosted_zone_id)를 각자 설정으로 대체합니다.
+# # 최초 설치 시 포함하지 않는 것을 권고합니다.
+# # 아래 TF 변수(ex var.cluster_name, var.secret_hosted_zone_id)를 각자 설정으로 대체합니다.
 
-# 혹은 아래와 같이 TF 변수를 설정합니다. 
-# export TF_VAR_secret_hosted_zone_id="xxxx"
-# export TF_VAR_secret_aws_account_id="xxxx"
-# export TF_VAR_cluster_name="xxxx"
+# # 혹은 아래와 같이 TF 변수를 설정합니다. 
+# # export TF_VAR_secret_hosted_zone_id="xxxx"
+# # export TF_VAR_secret_aws_account_id="xxxx"
+# # export TF_VAR_cluster_name="xxxx"
 
-module "load_balancer_controller_irsa_role" {
-  source = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
+# module "load_balancer_controller_irsa_role" {
+#   source = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
 
-  role_name                              = "load-balancer-controller-${var.cluster_name}"
-  attach_load_balancer_controller_policy = true
+#   role_name                              = "load-balancer-controller-${var.cluster_name}"
+#   attach_load_balancer_controller_policy = true
 
-  oidc_providers = {
-    ex = {
-      provider_arn               = module.eks.oidc_provider_arn
-      namespace_service_accounts = ["kube-system:aws-load-balancer-controller"]
-    }
-  }
+#   oidc_providers = {
+#     ex = {
+#       provider_arn               = module.eks.oidc_provider_arn
+#       namespace_service_accounts = ["kube-system:aws-load-balancer-controller"]
+#     }
+#   }
 
-  tags = local.tags
-}
+#   tags = local.tags
+# }
 
-module "external_dns_irsa_role" {
-  source = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
+# module "external_dns_irsa_role" {
+#   source = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
 
-  role_name                     = "external-dns-${var.cluster_name}"
-  attach_external_dns_policy    = true
-  external_dns_hosted_zone_arns = ["arn:aws:route53:::hostedzone/${var.secret_hosted_zone_id}"]
+#   role_name                     = "external-dns-${var.cluster_name}"
+#   attach_external_dns_policy    = true
+#   external_dns_hosted_zone_arns = ["arn:aws:route53:::hostedzone/${var.secret_hosted_zone_id}"]
 
-  oidc_providers = {
-    ex = {
-      provider_arn               = module.eks.oidc_provider_arn
-      namespace_service_accounts = ["kube-system:external-dns"]
-    }
-  }
+#   oidc_providers = {
+#     ex = {
+#       provider_arn               = module.eks.oidc_provider_arn
+#       namespace_service_accounts = ["kube-system:external-dns"]
+#     }
+#   }
 
-  tags = local.tags
-}
+#   tags = local.tags
+# }
 
-module "ebs_csi_irsa_role" {
-  source = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
+# module "ebs_csi_irsa_role" {
+#   source = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
 
-  role_name             = "ebs-csi-${var.cluster_name}"
-  attach_ebs_csi_policy = true
+#   role_name             = "ebs-csi-${var.cluster_name}"
+#   attach_ebs_csi_policy = true
 
-  oidc_providers = {
-    ex = {
-      provider_arn               = module.eks.oidc_provider_arn
-      namespace_service_accounts = ["kube-system:ebs-csi-controller-sa"]
-    }
-  }
+#   oidc_providers = {
+#     ex = {
+#       provider_arn               = module.eks.oidc_provider_arn
+#       namespace_service_accounts = ["kube-system:ebs-csi-controller-sa"]
+#     }
+#   }
 
-  tags = local.tags
-}
+#   tags = local.tags
+# }

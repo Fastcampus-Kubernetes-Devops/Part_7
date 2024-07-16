b8843c73aa75dc4bc57cf580915dfcfccc53d206
Jerry-levvels <jerry@levvels.io>
2024-07-16 14:13:47 +0900

    feat: comment variables on tf-eks

--- tf-eks/variables.tf
+++ tf-eks/variables.tf
@@ -1,11 +1,11 @@
-variable "secret_aws_account_id" {
-  type = string
-}
+# variable "secret_aws_account_id" {
+#   type = string
+# }
 
-variable "secret_hosted_zone_id" {
-  type = string
-}
+# variable "secret_hosted_zone_id" {
+#   type = string
+# }
 
-variable "cluster_name" {
-  type = string
-}
+# variable "cluster_name" {
+#   type = string
+# }
